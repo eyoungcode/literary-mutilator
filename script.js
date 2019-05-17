2cf8c59273513834883bddd8a54d17d85e283686
Erik Young <youngblkraven@gmail.com>
2019-05-17 17:12:33 -0600

    trying to highlight waistcoat

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 //styling for javascript challenge
 function textChanger(){
 	document.getElementById("one").style.fontFamily = "impact";
+	document.getElementById("one").style.color = "green";
 }
 function textColor(){
 	document.getElementById("two").style.color = "red";
+	document.getElementById("two").style.backgroundColor = "black";
+}
+function textWord(){
+	function textWord(text) {
+		var inputText = document.getElementById("three");
+		var innerHTML = inputText.innerHTML;
+		var index = innerHTML.indexOf(text);
+		if (index >= 0) {
+			innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+			inputText.innerHTML = innerHTML;
+		}
+	}
 }
